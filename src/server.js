a5a62eed9c1832e0ad370c9e1d2a3dc75f786b13
Fredd Bezerra <fredd@neosdev.com.br>
2019-04-14 22:07:17 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+//using express ...
+const express = require('express');
+//schema treatment for mondodb (schemaless)
+const mongoose = require('mongoose');
+// mongo conection done with mongoose
+// i'm using atlas to server mongo
+// TODO:  put this conection string to environment
+mongoose.connect(
+  'mongodb+srv://node-rocket:node-rocket@cluster0-2uuof.mongodb.net/rocket?retryWrites=true',
+    {
+        useNewUrlParser: true
+    }
+);
+const app = express();
+//parse json easy with express
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+//mvc pattern ...
+app.use(require('./routes'));
+
+
+
+app.listen(3333);
+ 
